2990cdf9d289023463d77cd5357760cfeeaddd7c
Andrei Dochkin <andreydochkin@yandex.ru>
2023-09-04 15:32:13 +0400

    test: cors again

--- app.js
+++ app.js
@@ -40,10 +40,10 @@ app.use(cors({
 
 // app.use(cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 
 // Set security headers
 
