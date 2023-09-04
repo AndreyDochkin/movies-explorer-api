a3d315ea307a556e9d38888a20da1594e378faee
Andrei Dochkin <andreydochkin@yandex.ru>
2023-09-04 12:22:11 +0400

    fix: origin cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(requestLogger);
 
 app.use(cors({
   // origin:'*'
-  [
+  origin: [
     'https://localhost:3000',
     'http://localhost:3000',
     'http://moviematchup.nomoreparties.sbs',
